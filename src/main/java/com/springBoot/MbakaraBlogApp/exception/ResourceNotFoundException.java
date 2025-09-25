8d4cd82428a08e737a1e010dd5ba23c1259a0c7d
GloryUsen <ganwana89@gmail.com>
2025-09-25 10:40:47 +0100

    making changes

--- src/main/java/com/springBoot/MbakaraBlogApp/exception/ResourceNotFoundException.java
+++ src/main/java/com/springBoot/MbakaraBlogApp/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package com.springBoot.MbakaraBlogApp.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
 
     private String resourceName;
